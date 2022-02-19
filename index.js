1c8bc14e3ad54d4b598c24ea4c339f7b338365d2
Sumit kumar <sumitkumar@Sumits-MacBook-Air.local>
2022-02-19 18:49:24 +0530

    product added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 5000;
 
 var client_id = "8e9ed7adc9bd4ed2b981197b8a7029c9";
 var client_secret = "9bcc940d0001494186f823b34f0a6345";
-var redirect_uri = "http://localhost:5000/callback";
+var redirect_uri = "https://spotify-home.herokuapp.com/callback";
 
 app.get("/login", (req, res) => {
   var state = generate();
@@ -59,7 +59,7 @@ app.get("/callback", function (req, res) {
       if (!error && response.statusCode === 200) {
         const { access_token, refresh_token } = body;
         res.redirect(
-          `http://127.0.0.1:5500/?access_token=${access_token}&refresh_token=${refresh_token}`
+          `https://ammyy9908.github.io/spotifyhome/?access_token=${access_token}&refresh_token=${refresh_token}`
         );
       }
     });
